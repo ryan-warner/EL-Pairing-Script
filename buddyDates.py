62f8849dc111ad50b0ee8857206b53a699417523
Ryan Warner <ryan.warner.us@gmail.com>
2022-07-06 15:46:10 +0200

    Account for advisor exclusion from pairing

--- buddyDates.py
+++ buddyDates.py
@@ -5,41 +5,61 @@ import random
 
 ## Script Options ##
 numWeeks = 26
-allowRecycledPairs = False
+useRecycledPairs = False
 useOldPairings = True
 
 ## File Information ##
 outputFilePath = "Buddy Dates - EL 2023-2024 Spring.csv"
 oldPairingsPath = "Buddy Dates - EL 2023-2024 Fall.csv"
 emergelingsPath = "roster.csv"
+advisorsPath = "advisors.csv"
 
 ## Function to gather unique pairs ##
-def getNewPair(optionsList, oldPairingsList, allowRecycledPairs):
-    foundNewPair = False
-    counter = 1
-    tempList = []
-    while not foundNewPair and counter <= comb(len(optionsList), 2):
-        newCombination = False
-        pairing = None
-        reversedPairing = None
-        allowableException = False
-        while not newCombination:
-            pairing = random.sample(optionsList, 2)
-            reversedPairing = pairing[::-1]
-            if pairing not in tempList and reversedPairing not in tempList:
-                newCombination = True
-            elif counter == comb(len(optionsList), 2) and allowRecycledPairs:
-                pairing = random.sample(optionsList, 2)
-                reversedPairing = pairing[::-1]
-                allowableException = True
+def getNewPair(optionsList, advisorsList, oldPairingsList, allowRecycledPairs):
+    allowableException = False
+    forceExit = False
 
-        if (pairing not in oldPairingsList and reversedPairing not in oldPairingsList) or allowableException:
-            foundNewPair = True
+    counter = 1
+    tempList = [] # Temp list contains the combinations that have been tried
+    while counter <= comb(len(optionsList), 2):
+        if counter == comb(len(optionsList), 2) and allowRecycledPairs:
+            allowableException = True
+        elif counter == comb(len(optionsList), 2):
+            forceExit = True
+               
+        pairing = random.sample(optionsList, 2)
+        checkMessage = checkPairing(pairing, advisorsList, oldPairingsList, tempList, allowableException)
+        if checkMessage == "Match OK":
             return pairing
-        else:
+        elif checkMessage == "Already tried":
+            if not allowRecycledPairs and forceExit:
+                counter += 1
+            else:
+                pass
+        elif checkMessage == "Advisors may not be matched":
+            tempList.append(pairing)
+            counter += 1
+        elif checkMessage == "Already matched - recycled pairs allowed":
+            return pairing
+        elif checkMessage == "Already matched":
             tempList.append(pairing)
             counter += 1
     return None
+
+## Function to check if pairing is unique ##
+def checkPairing(pairing, advisorsList, oldPairingsList, tempList, allowRecycledPairs):
+    reversedPairing = pairing[::-1]
+    if (pairing in tempList or reversedPairing in tempList) and not allowRecycledPairs:
+        return "Already tried"
+    elif pairing[0] in advisorsList and pairing[1] in advisorsList:
+        return "Advisors may not be paired"
+    elif pairing in oldPairingsList or reversedPairing in oldPairingsList:
+        if allowRecycledPairs:
+            return "Already matched - recycled pairs allowed"
+        else:
+            return "Already matched"
+    else:
+        return "Match OK"
 
 ## Function to write weekly pairings to a file ##
 def writePairings(pairings, weekNum, writer):
@@ -61,14 +81,25 @@ def getOldPairings():
 ## Main Program ##
 
 if not exists(emergelingsPath):
-    print("Error: The provided roster does not exist. Please double check that the file is in the same directory as this script, and that you have provided the correct name.")
+    print("Error: The provided Emergeling roster does not exist. Please double check that the file is in the same directory as this script, and that you have provided the correct name.")
     exit()
 
-with open(emergelingsPath, 'r') as inputFile:
+if not exists(advisorsPath):
+    print("Error: The provided advisors roster does not exist. Please double check that the file is in the same directory as this script, and that you have provided the correct name.")
+    exit()
+
+with open(emergelingsPath, 'r') as emergelingsInput:
     # read each line of the csv file into an array of names
-    reader = csv.reader(inputFile)
-    names = list(reader)
-    numEmergelings = len(names)
+    reader = csv.reader(emergelingsInput)
+    emergelings = list(reader)
+
+with open(advisorsPath, 'r') as advisorsInput:
+    # read each line of the csv file into an array of names
+    reader = csv.reader(advisorsInput)
+    advisors = list(reader)
+
+names = emergelings + advisors
+numMembers = len(names)
 
 ## Include old.csv in the directory when running if you want to account for old pairings ##
 if useOldPairings:
@@ -89,9 +120,9 @@ currentWeek = 1
 
 ## Pairing Loop ##
 while currentWeek <= numWeeks:
-    while counter < numEmergelings / 2:
+    while counter < numMembers / 2:
         # get a random pairing from the names array
-        pairing = getNewPair(names, completePairings, allowRecycledPairs)
+        pairing = getNewPair(names, advisors, completePairings, useRecycledPairs)
         if pairing is not None:
             result.append(pairing)
             completePairings.append(pairing)
@@ -101,6 +132,7 @@ while currentWeek <= numWeeks:
     if (len(result) == 0):
         writer.writerow(["Maximum combinations reached. Consider allowing recycled pairs."])
     writer.writerow("")
+    print("Week " + str(currentWeek) + " pairings complete.")
     currentWeek += 1
     counter = 0
     result = []
