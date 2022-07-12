629876edfbf156da5e97ab6613647bc6d3eedbbd
Ryan Warner <ryan.warner.us@gmail.com>
2022-07-12 10:43:08 +0200

    Updated test, block and enable printing

--- test.py
+++ test.py
@@ -1,10 +1,22 @@
 counter = 1
 import buddyDates
+import sys, os
 
+## Uncomment main method in buddyDates.py to test ##
 def runScript():
+    blockPrint()
     buddyDates.main()
+    enablePrint()
 
-while counter <= 10:
+# Disable Printing
+def blockPrint():
+    sys.stdout = open(os.devnull, 'w')
+
+# Restore Printing
+def enablePrint():
+    sys.stdout = sys.__stdout__
+
+while counter <= 100:
     runScript()
     print("Run " + str(counter) + " complete")
     counter += 1
